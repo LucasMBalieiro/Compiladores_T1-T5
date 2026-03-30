4766f7310e3b4ed14b48266aa8e903a2d4e700c4
Lucas Balieiro <lucas.macbali@gmail.com>
2026-03-30 00:55:02 -0300

    Reescrevendo-T1

--- T1/src/main/java/org/ufscar/Main.java
+++ T1/src/main/java/org/ufscar/Main.java
@@ -1,16 +1,8 @@
 package org.ufscar;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args)
     {
-        AlgumaLexico lex = new AlgumaLexico(args[0]);
 
-        Token t = null;
-
-        while ((t = lex.ProximoToken()) != null)
-        {
-            System.out.print(t);
-        }
     }
 }
